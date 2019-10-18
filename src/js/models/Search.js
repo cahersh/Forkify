b73bc9a5dc665cff7d2c7aeb48f51946d9ad6d00
Chandler <chanalexberger@gmail.com>
2019-10-18 18:05:46 -0400

    started build of Search controller

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -6,7 +6,7 @@ export default class Search {
     }
 
     async getResults() {
-        const key = 'aaaaaaaaaaaaaaaaa34c9f2aabd9c06a05e603615806facde';
+        const key = '34c9f2aabd9c06a05e603615806facde';
         try {
             const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
@@ -14,8 +14,7 @@ export default class Search {
         } catch(error) {
             //alert(error);
             console.log(error);
-        }
-        
+        }   
     }
 }
 
