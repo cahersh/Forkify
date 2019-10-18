e1410e58cbfc93c19292814dcf16674b4dd3f7d1
Chandler <chanalexberger@gmail.com>
2019-10-18 17:52:21 -0400

    built the Search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const key = 'aaaaaaaaaaaaaaaaa34c9f2aabd9c06a05e603615806facde';
-    try {
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes
-        console.log(res);
-    } catch(error) {
-        alert(error);
-    }
-    
-}
-getResults('pizza');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
