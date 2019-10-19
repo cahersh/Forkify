274f8cf8768a8b482a87a52615f20a92403a35d1
Chandler <chanalexberger@gmail.com>
2019-10-19 10:14:04 -0400

    implemented limitRecipeTitle to reduce recipe title to a single line

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -13,6 +13,29 @@ export const clearResults = () => {
     elements.searchResultList.innerHTML = '';
 };
 
+// Purpose: limit length of recipe title but don't split individual words
+// Inputs: title - title of the recipe, limit - character limit for title
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    // Test if title is longer than limit
+    if (title.length > limit) {
+        //Split string by spaces into array - 'Pasta with tomato and spinach' => ['Pasta','with','tomato','and','spinach']
+        title.split(' ').reduce((acc, cur) => {
+            // Add values from array until limit is reached
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0); 
+
+        // return new title and join newTitle with spaces
+        return `${newTitle.join(' ')} ...`;
+    }
+
+    // else - return the title
+    return title;
+};
+
 // Purpose: Renders a single recipe - adds single recipe to result list
 // Input: a single recipe
 const renderRecipe = recipe => {
@@ -24,7 +47,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
